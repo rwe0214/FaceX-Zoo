971b4564c02aa554008323d0a6dd6412b432f8e4
Shao-Wei Chiu <shwchiu@cs.nctu.edu.tw>
2021-07-16 14:35:19 +0800

    Add crop_size and crop_mode
    Add variable to configure size and mode of cropping image and update the repo path

--- test_protocol/ijbc/face_cropper/crop_ijbc_by_arcface.py
+++ test_protocol/ijbc/face_cropper/crop_ijbc_by_arcface.py
@@ -10,10 +10,10 @@ import math
 import multiprocessing
 
 import cv2
-sys.path.append('/home/wangjun492/FaceX-Zoo/face_sdk')
+sys.path.append('/home/swchiu/workspace/FaceX-Zoo/face_sdk')
 from core.image_cropper.arcface_cropper.FaceRecImageCropper import FaceRecImageCropper
 
-def crop_ijbc(ijbc_root, pts_score_file, target_folder):
+def crop_ijbc(ijbc_root, pts_score_file, target_folder, crop_size=112, mode='arcface'):
     face_cropper = FaceRecImageCropper()
     pts_score_file_buf = open(pts_score_file)
     line = pts_score_file_buf.readline().strip()
@@ -25,13 +25,17 @@ def crop_ijbc(ijbc_root, pts_score_file, target_folder):
         cur_image = cv2.imread(image_path)        
         image_lmk = line_strs[1:11]
         face_lms = [float(s) for s in image_lmk]
-        cur_cropped_image = face_cropper.crop_image_by_mat(cur_image, face_lms)
+        cur_cropped_image = face_cropper.crop_image_by_mat(cur_image, face_lms, crop_size, mode)
         cv2.imwrite(target_path, cur_cropped_image)
         line = pts_score_file_buf.readline().strip()
         
 if __name__ == '__main__':
-    ijbc_root = '/home/wangjun492/notespace/IJB_release/IJBC/loose_crop'
-    pts_score_file = '/home/wangjun492/notespace/IJB_release/IJBC/meta/ijbc_name_5pts_score.txt'    
-    target_folder = '/home/wangjun492/notespace/IJB_release/IJBC/arcface_crop'
+    crop_size = 112
+    mode = 'arcface'
+    if mode == 'arcface':
+        assert crop_size==112
+    ijbc_root = '/mnt/disk1/IJB_release/IJBC/loose_crop'
+    pts_score_file = '/mnt/disk1/IJB_release/IJBC/meta/ijbc_name_5pts_score.txt'    
+    target_folder = f'/mnt/disk1/IJB_release/IJBC/arcface_crop_{crop_size}'
 
-    crop_ijbc(ijbc_root, pts_score_file, target_folder)
+    crop_ijbc(ijbc_root, pts_score_file, target_folder, crop_size, mode)
